105deea4760cd921b796e6a6a7ab2d79d357c8d5
kopylovsg <kopylovsg@ya.ru>
2022-11-11 13:39:25 +0700

    attestat_2

--- atestatJS/script.js
+++ atestatJS/script.js
@@ -1,70 +1,70 @@
-const mailErrorData = document.querySelector('[data-js-mail-title]');
+const mailErrorData = document.querySelector('[data-js-mail-error]');
 const emailData = document.querySelector('[data-js-mail]');
-const passwordErrorData = document.querySelector('[data-js-password-title]');
+const passwordErrorData = document.querySelector('[data-js-password-error]');
 const passwordData = document.querySelector('[data-js-password]');
-const passwordVerErrorData = document.querySelector('[data-js-password-title]');
+const passwordVerErrorData = document.querySelector('[data-js-passwordVer-error]');
 const passwordVerData = document.querySelector('[data-js-passwordVer]');
 const colorError = 'red';
-const applicantForm = document.getElementById('formSubmit')
-applicantForm.addEventListener('submit', handleFormSubmit)
+const errorLength = "Поле обязательно для заполнения";
 
-  function handleFormSubmit(event) {
-    // Просим форму не отправлять данные самостоятельно
-    event.preventDefault()
-    console.log('Отправка!')
+const applicantForm = document.getElementById('formSubmit')
+applicantForm.addEventListener('submit', handleFormSubmit);
+
+function serializeForm(formNode) {
+  const { elements } = formNode;
+
+  const data = Array.from(elements)
+    .map((element) => {
+      const {name, type} = element;
+      const volue = type === 'checkbox' ? element.checked : element.value;
+
+
+      return {name, volue};
+    })
+    .filter((item) => !!item.name);
+
+  console.log(data);
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
     const validatMail = emailData.value;
     const validatPassvord = passwordData.value;
     const validatPassvordVer = passwordVerData.value;
 
-
-     console.log(validatMail);
-     console.log(validatPassvord);
-     console.log(validatPassvordVer);
+    function validateEmail(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    };
 
     if (!validatMail.length) {
-        emailData.style.borderColor = colorError;
-    } else {
-      function validateEmail(emailData) {
-          const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*) | (".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          return re.test(String(emailData).toLowerCase());
-           console.log(re);
-        }
-    }
+      emailData.style.borderColor = colorError;
+      document.querySelector('[data-js-mail-error]').innerHTML = errorLength;
+    } else if (!validateEmail(validatMail)) {
+      document.querySelector('[data-js-mail-error]').innerHTML = "Email введён некорректно";
+      emailData.style.borderColor = colorError;
+    };
+
     if (!validatPassvord.length) {
       passwordData.style.borderColor = colorError;
-    }
-    if (!validatPassvordVer.length) {
+      document.querySelector('[data-js-password-error]').innerHTML = errorLength;
+      return;
+    };
+
+    if (validatPassvord.length < 8) {
+      passwordData.style.borderColor = colorError;
+      document.querySelector('[data-js-password-error]').innerHTML = "Пароль должен содержать не менее 8 символов";
+    };
+
+    if (!validatPassvordVer.length || validatPassvordVer.length < 8 || validatPassvord !== validatPassvordVer) {
       passwordVerData.style.borderColor = colorError;
-    }
+      document.querySelector('[data-js-passwordVer-error]').innerHTML = "Пароли не совпадают";
+      return;
+    };
 
-    // if (passwordData !=== passwordVerData)
-    //   passwordVerErrorData.style.text
-    //     return;
-    //     }
-
-
+    serializeForm(applicantForm)
+};
 
 
-
-
-
-  }
-
-
-
-  //
-  //
-  //
-  //
-  //
-  //  function validateEmail(emailData) {
-  //   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*) | (".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  //   return re.test(String(emailData).toLowerCase());
-  //    console.log(re);
-  // }
-
-  // if (validatePassword)
-  //   passwordData !=== passwordVerData
-  //   return;
-  //   }
 
